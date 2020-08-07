65ca381f3190301536512c5df9ac537509c3af1b
dxiaoxing <1693992774@qq.com>
2020-08-07 14:14:21 +0800

    非项目代码之node

---
+++ Node相关/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: dxiaoxing
+ * @Date: 2020-08-07 10:51:49
+ * @LastEditors: dxiaoxing
+ * @LastEditTime: 2020-08-07 11:44:53
+ */
+let $ = require('jquery') 
+console.log($);
