af8eb87a0c95434bb99e45dfbb26f2d9e4c7cc44
dxiaoxing <1693992774@qq.com>
2020-09-21 19:01:50 +0800

    webpack小demo开头

---
+++ webpack相关/01/index.js
@@ -0,0 +1,11 @@
+
+var dom = document.getElementById('root')
+var header = document.createElement('div')
+header.innerText = 'header'
+dom.append(header)
+
+
+var sidebar = document.getElementById('root')
+var sidebar = document.createElement('div')
+sidebar.innerText = 'sidebar'
+dom.append(sidebar)
