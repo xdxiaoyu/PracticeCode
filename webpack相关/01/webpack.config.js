ebf016c8964c2eda0687cdd07883409b7d688a63
dxiaoxing <1693992774@qq.com>
2020-09-23 19:34:07 +0800

    webpack安装，基本配置文件了解

---
+++ webpack相关/01/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+// node核心模块path两个小知识点复习
+// __dirname：或取当前执行文件所在目录的完整目录名
+// path.resolve()：把路径或者路径片段的序列解析为一个绝对路径
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
