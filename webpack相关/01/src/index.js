3b12a44014d1956001bed40d1d298fef5df79bf4
dxiaoxing <1693992774@qq.com>
2020-09-25 18:34:46 +0800

    loader简述，打包的一种方案

--- webpack相关/01/src/index.js
+++ webpack相关/01/src/index.js
@@ -1,10 +1,19 @@
-import Header from './header'
-import Sidebar from './sidebar'
-import Content from './content'
+// import Header from './header'
+// import Sidebar from './sidebar'
+// import Content from './content'
+// var two = require('./2.jpg')
+
+// console.log(two);
 
 
+// new Header()
+// new Sidebar()
+// new Content()
 
+import two from './2.jpg'
 
-new Header()
-new Sidebar()
-new Content()
+var img = new Image()
+img.src = two
+
+var root = document.getElementById('root')
+root.append(img)
--- webpack相关/01/webpack.config.js
+++ webpack相关/01/webpack.config.js
@@ -6,6 +6,14 @@ const path = require('path')
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  module: {
+    rules: [{
+      test: /\.jpg$/,
+      use: {
+        loader: 'file-loader'
+      }
+    }]
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
