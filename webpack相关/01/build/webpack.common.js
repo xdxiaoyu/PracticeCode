86e59a4b475bf2a02bcd437589dfd01e3cf4ab98
dxiaoxing <1693992774@qq.com>
2020-11-06 18:07:07 +0800

    SplitChunks插件学习

--- webpack相关/01/build/webpack.common.js
+++ webpack相关/01/build/webpack.common.js
@@ -71,11 +71,35 @@ module.exports = {
     new CleanWebpackPlugin(),
   ],
   optimization: {
+    /*  
     splitChunks: {
       chunks: 'all',
       cacheGroups: {
-        defaultVendors: false,
+        vendors: false,
         default: false
+      }
+    }
+    */
+    splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      // minRemainingSize: 0,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
       }
     }
   },
