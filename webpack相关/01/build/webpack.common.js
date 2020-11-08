4141180b44892ae709a40c418fd90742446dc643
dxiaoxing <1693992774@qq.com>
2020-11-08 18:01:33 +0800

    更改配置信息

--- webpack相关/01/build/webpack.common.js
+++ webpack相关/01/build/webpack.common.js
@@ -91,15 +91,8 @@ module.exports = {
       automaticNameDelimiter: '~',
       enforceSizeThreshold: 50000,
       cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        }
+        vendors: false,
+        default: false
       }
     }
   },
