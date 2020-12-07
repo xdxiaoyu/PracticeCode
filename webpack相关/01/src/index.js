d76ff2f801302c66e883c7eb8104f4bac5d9f65d
dxiaoxing <1693992774@qq.com>
2020-12-07 21:32:30 +0800

    Shimming开头

--- webpack相关/01/src/index.js
+++ webpack相关/01/src/index.js
@@ -209,7 +209,9 @@ root.append(img)
 // Shimming
 import $ from 'jquery';
 import _ from 'lodash';
+import { ui } from './jquery.ui'
 
+ui()
 
 const dom = $('<div>');
 dom.html(_.join(['dell', 'lee'], '----'));
