081230fdbd42fe949925a20e34f20b9a8f29d292
dxiaoxing <1693992774@qq.com>
2020-12-21 18:30:09 +0800

    library打包配置讲解

--- webpack相关/libary/webpack.config.js
+++ webpack相关/libary/webpack.config.js
@@ -3,7 +3,14 @@ const path = require('path')
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
-  externals: ['lodash'],
+  // externals: ['lodash'],
+  // externals: {
+  //   lodash: {
+  //     root: '_',
+  //     commonjs: 'lodash'
+  //   }
+  // },
+  externals: 'lodash', // lodash在打包库的时候，不打包到库的代码里面去，而是让业务代码加载。
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'library.js',
@@ -14,9 +21,8 @@ module.exports = {
     //     const library = reqyire('library')
     //     require(['library'], function() {})
 
-    // 想用下面这种方式引入，<script src='library.js'></script>
-    // 全局使用 library.math
-    library: 'library',
+    // 想用下面这种方式引入，<script src='library.js'></script>,全局使用 library.math
+    library: 'library', // 可以命名为其他字符串名称
     // 打包生成的代码挂载到一个页面的全局上library，配置了一个'library'参数
 
     // libraryTarget: 'this' // 'window'  
