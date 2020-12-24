e2979f6e497c1e473c2078ecb1b5f0031a0fba54
dxiaoxing <1693992774@qq.com>
2020-12-24 14:11:35 +0800

    plugin钩子函数初步使用

---
+++ webpack相关/plugin/plugins/copyright-webpack-plugin.js
@@ -0,0 +1,28 @@
+class CopyrightWebpackPlugin {
+  // constructor(options) {
+  //   console.log('插件被使用了',options);
+  // }
+
+  apply(compiler) {
+    // compiler - 存放打包所有相关的内容
+    // 钩子。emit: 生成资源到 output 目录之前。
+    // tapAsync: 接收两个参数，这个plugin的名字和回调函数
+    compiler.hooks.emit.tapAsync('CopyrightWebpackPlugin',(compilation, cb) => { // compilation - 存放这次打包的相关内容
+      compilation.assets['copyright.txt'] = {
+        source: function() {
+          return 'copyright by dell lee'
+        },
+        size: function() {
+          return 21
+        }
+      };
+      cb()
+    })
+
+    compiler.hooks.compile.tap('CopyrightWebpackPlugin', (compilation) => {
+      console.log('compiler');
+    })
+  }
+}
+
+module.exports = CopyrightWebpackPlugin
