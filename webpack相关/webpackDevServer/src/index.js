3caa961c144ea2544383e5b8c42470d9ed6bb91b
dxiaoxing <1693992774@qq.com>
2021-01-02 16:18:02 +0800

    webpackDevServer请求转发配置

---
+++ webpack相关/webpackDevServer/src/index.js
@@ -0,0 +1,21 @@
+import "@babel/polyfill";
+
+import React, { Component } from 'react';
+import ReactDom from 'react-dom'
+import axios from 'axios'
+
+class App extends Component {
+
+  componentDidMount() {
+    // charles fiddler
+    axios.get('/react/api/header.json')
+        .then((res) => {
+          console.log('结果：', res);
+        })
+  }
+  render() {
+    return <div>Hello World </div>
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
