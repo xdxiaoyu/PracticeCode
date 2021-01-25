a5d193c7a96df42d0af1be223e7d93b6cfa48e4f
dxiaoxing <1693992774@qq.com>
2021-01-25 18:10:13 +0800

    编写一个简单的代码打包工具

---
+++ webpack相关/bundler/bundle.js
@@ -0,0 +1,27 @@
+// 实现简单的打包功能
+
+const fs = require('fs')
+const parser = require('@babel/parser')
+const traverse = require('@babel/traverse').default
+
+const moduleAnalyser = (filename) => {
+  const content = fs.readFileSync(filename, 'utf-8')
+  /* 
+  console.log(parser.parse(content, {
+    sourceType: 'module'
+  })); // 打印出来的是抽象语法树（AST）
+  */
+  const ast = parser.parse(content, {
+    sourceType: 'module'
+  })
+  const dependencies = [];
+  traverse(ast, {
+    ImportDeclaration({ node }) {
+      console.log(node);
+    }
+  })
+  // console.log(ast.program.body);
+}
+
+moduleAnalyser('./src/index.js')
+
---
+++ webpack相关/bundler/src/index.js
@@ -0,0 +1,4 @@
+import message from './message.js'
+import message1 from './message1.js'
+
+console.log(message)
