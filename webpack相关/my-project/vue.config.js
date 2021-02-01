553aa14b7c2f417aa0448a34f88243632b40a53d
dxiaoxing <1693992774@qq.com>
2021-02-01 18:24:25 +0800

    vue-cli中webpack相关配置了解

--- webpack相关/my-project/vue.config.js
+++ webpack相关/my-project/vue.config.js
@@ -1,3 +1,10 @@
+const path = require('path')
+
 module.exports = {
-  outputDir: 'dell',
+  // outputDir: 'dell',
+  // configureWebpack: {
+    devServer: {
+      contentBase: [path.join(__dirname, 'static')]
+    }
+  // }
 }
