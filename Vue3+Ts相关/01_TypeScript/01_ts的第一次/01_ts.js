8f436fa38854d073def220e37c332b515f356c16
丁星星 <1693992774@qq.com>
2023-05-23 22:43:53 +0800

    feat：新增ts初学代码

---
+++ Vue3+Ts相关/01_TypeScript/01_ts的第一次/01_ts.js
@@ -0,0 +1,7 @@
+(function () {
+    function sayHi(str) {
+        return 'Hello Word TypeScript' + str;
+    }
+    var text = '小甜甜';
+    console.log(sayHi(text));
+})();
---
+++ Vue3+Ts相关/01_TypeScript/01_ts的第一次/01_ts.ts
@@ -0,0 +1,8 @@
+(() => {
+  function sayHi(str:string) {
+    return 'Hello Word TypeScript' + str
+  }
+
+  let text = '小甜甜'
+  console.log(sayHi(text))
+})()
